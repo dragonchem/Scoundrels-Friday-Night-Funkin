590eaca5f4a1e40ca7e40a3b8d506f6a66690639
Wout van Riel <wout@student.fontys.nl>
2021-04-15 09:14:42 +0200

    discord :)

--- source/Discord.hx
+++ source/Discord.hx
@@ -12,7 +12,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "557069829501091850", // change this to what ever the fuck you want lol
+			clientID: "831943661943390238", // change this to what ever the fuck you want lol
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
@@ -39,8 +39,8 @@ class DiscordClient
 		DiscordRpc.presence({
 			details: "In the Menus",
 			state: null,
-			largeImageKey: 'icon',
-			largeImageText: "fridaynightfunkin"
+			largeImageKey: 'testimage',
+			largeImageText: "This Is A Test"
 		});
 	}
 
