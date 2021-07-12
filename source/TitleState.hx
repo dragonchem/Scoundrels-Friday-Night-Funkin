eae10c89c923be9bb92aaae58ba71b9c810bf75b
Wout van Riel <wout@student.fontys.nl>
2021-07-12 23:38:19 +0200

    title animation

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -165,6 +165,7 @@ class TitleState extends MusicBeatState
 
 		logoBl = new FlxSprite();
 		logoBl.frames = Paths.getSparrowAtlas('logoBumpin');
+		logoBl.scale.set(0.75, 0.75);
 		logoBl.antialiasing = true;
 		logoBl.animation.addByPrefix('bump', 'logo bumpin', 24);
 		logoBl.animation.play('bump');
@@ -172,10 +173,11 @@ class TitleState extends MusicBeatState
 		// logoBl.screenCenter();
 		// logoBl.color = FlxColor.BLACK;
 
-		gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
-		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
-		gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
-		gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+		gfDance = new FlxSprite(FlxG.width * 0.35, FlxG.height * 0.12);
+		gfDance.frames = Paths.getSparrowAtlas('speakerBump');
+		gfDance.scale.set(0.95, 0.95);
+		gfDance.animation.addByPrefix('beat', 'HatsSpeaker', 24);
+		// gfDance.animation.addByIndices('danceRight', 'HatsSpeaker', [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 		gfDance.antialiasing = true;
 		add(gfDance);
 		add(logoBl);
@@ -378,12 +380,13 @@ class TitleState extends MusicBeatState
 		super.beatHit();
 
 		logoBl.animation.play('bump');
-		danceLeft = !danceLeft;
+		gfDance.animation.play('beat');
+		// danceLeft = !danceLeft;
 
-		if (danceLeft)
-			gfDance.animation.play('danceRight');
-		else
-			gfDance.animation.play('danceLeft');
+		// if (danceLeft)
+		// 	gfDance.animation.play('danceRight');
+		// else
+		// 	gfDance.animation.play('danceLeft');
 
 		FlxG.log.add(curBeat);
 
